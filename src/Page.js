3dd831118b91e543fd60385dac4babd57daf6e53
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-08-09 17:48:25 +0200

    some style tweeks

--- src/Page.js
+++ src/Page.js
@@ -4,19 +4,16 @@ import Copyright from './components/Copyright'
 import Header from './components/Header'
 import useStyles from './useStyles'
 import { useStoreActions, useStoreState } from 'easy-peasy'
-import { Container, CssBaseline, Grid, Box } from '@material-ui/core'
+import { Container, Grid, Box } from '@material-ui/core'
 
 export default () => {
   const classes = useStyles()
   const repos = useStoreState((state) => state.repos.items)
   const starRepo = useStoreActions((actions) => actions.repos.starRepo)
   return (
-    <Container className={classes.root}>
-      <CssBaseline />
-      <Grid container spacing={1}>
-        <Header />
-        <ReposGrid {...{ repos, starRepo }} />
-      </Grid>
+    <Container>
+      <Header />
+      <ReposGrid {...{ repos, starRepo }} />
       <Box mt={8}>
         <Copyright />
       </Box>
